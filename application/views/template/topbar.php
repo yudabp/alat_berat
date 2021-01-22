8e98214f4679db3189e6100a354c82a23b30f8bd
yudabp <tama.yuda48@gmail.com>
2021-01-22 08:52:28 +0700

    ubah logo sidebar

--- application/views/template/topbar.php
+++ application/views/template/topbar.php
@@ -39,8 +39,9 @@
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-        <a class="navbar-brand brand-logo" href="<?php echo base_url(); ?>">
-          <img src="<?php echo base_url(); ?>assets/chroma/images/logo.svg" alt="logo" />
+        <a class="navbar-brand brand-logo" href="<?php echo base_url(); ?>" style="margin: 15px;">
+          <!-- <img src="<?php echo base_url(); ?>assets/chroma/images/logo.svg" alt="logo" /> -->
+          <h3>XAVAXX</h3>
         </a>
         <a class="navbar-brand brand-logo-mini" href="<?php echo base_url(); ?>">
           <img src="<?php echo base_url(); ?>assets/chroma/images/logo-mini.svg" alt="logo" />
